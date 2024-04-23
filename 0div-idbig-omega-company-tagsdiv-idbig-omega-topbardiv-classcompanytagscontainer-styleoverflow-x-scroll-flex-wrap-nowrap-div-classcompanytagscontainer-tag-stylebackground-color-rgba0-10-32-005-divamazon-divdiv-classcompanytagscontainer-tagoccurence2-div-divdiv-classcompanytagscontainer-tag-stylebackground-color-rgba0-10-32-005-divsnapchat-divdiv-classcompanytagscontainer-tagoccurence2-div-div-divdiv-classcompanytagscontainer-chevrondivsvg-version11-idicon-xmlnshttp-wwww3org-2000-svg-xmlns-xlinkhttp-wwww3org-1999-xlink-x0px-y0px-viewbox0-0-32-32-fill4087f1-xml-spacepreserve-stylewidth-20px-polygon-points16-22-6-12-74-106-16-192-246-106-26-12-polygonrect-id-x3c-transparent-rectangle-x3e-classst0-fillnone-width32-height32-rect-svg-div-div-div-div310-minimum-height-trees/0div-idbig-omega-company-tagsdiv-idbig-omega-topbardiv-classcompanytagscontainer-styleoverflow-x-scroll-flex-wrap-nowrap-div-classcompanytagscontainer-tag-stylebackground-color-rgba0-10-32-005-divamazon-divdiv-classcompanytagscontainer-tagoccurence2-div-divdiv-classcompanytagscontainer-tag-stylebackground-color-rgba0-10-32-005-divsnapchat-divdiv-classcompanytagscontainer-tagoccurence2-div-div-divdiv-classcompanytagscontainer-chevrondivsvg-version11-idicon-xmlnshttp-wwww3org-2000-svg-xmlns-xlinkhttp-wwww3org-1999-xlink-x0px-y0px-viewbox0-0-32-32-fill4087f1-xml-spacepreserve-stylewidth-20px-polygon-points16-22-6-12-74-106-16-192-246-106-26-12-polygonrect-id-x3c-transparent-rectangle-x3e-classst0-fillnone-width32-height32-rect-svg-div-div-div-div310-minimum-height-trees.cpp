fecf34fcd5f4e74a8a41cba85d0dbe2f148cd0d4
princeraj2003 <126746978+princeraj2003@users.noreply.github.com>
2024-04-23 20:18:50 +0530

    Time: 102 ms (81.11%), Space: 55.4 MB (87.96%) - LeetHub

--- 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsnapchat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div310-minimum-height-trees/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsnapchat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div310-minimum-height-trees.cpp
+++ 0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsnapchat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div310-minimum-height-trees/0div-idbig-omega-company-tagsdiv-idbig-omega-topbardiv-classcompanytagscontainer-styleoverflow-x-scroll-flex-wrap-nowrap-div-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divamazon-divdiv-classcompanytagscontainer-tagoccurence2-div-divdiv-classcompanytagscontainer-tag-stylebackground-color-rgba0-10-32-005-divsnapchat-divdiv-classcompanytagscontainer-tagoccurence2-div-div-divdiv-classcompanytagscontainer-chevrondivsvg-version11-idicon-xmlnshttp-wwww3org-2000-svg-xmlns-xlinkhttp-wwww3org-1999-xlink-x0px-y0px-viewbox0-0-32-32-fill4087f1-xml-spacepreserve-stylewidth-20px-polygon-points16-22-6-12-74-106-16-192-246-106-26-12-polygonrect-id-x3c-transparent-rectangle-x3e-classst0-fillnone-width32-height32-rect-svg-div-div-div-div310-minimum-height-trees.cpp
@@ -1,38 +1,39 @@
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<vector<int>>& edges) {
-        vector<vector<int>>graph(n);
-        vector<int>indegree(n,0),ans;
-        for(auto it:edges){
-            graph[it[0]].push_back(it[1]);
-            graph[it[1]].push_back(it[0]);
-            indegree[it[0]]++;
-            indegree[it[1]]++;
-
+        if(n==1)return {0};
+        vector<vector<int>>adj(n);
+        vector<int>degree(n);
+        for(vector<int>&it:edges){
+            int from=it[0],to=it[1];
+            adj[from].push_back(to);
+            adj[to].push_back(from);
+            degree[from]++;
+            degree[to]++;
         }
-queue<int>q;
-for(int i=0;i<n;i++){
-    if(indegree[i]==1){
-        q.push(i);
-        indegree[i]--;
-    }
-}
-while(!q.empty()){
-    int s=q.size();
-    ans.clear();
-    for(int i=0;i<s;i++){
-        int curr=q.front();
-        q.pop();
-        ans.push_back(curr);
-        for(auto child:graph[curr]){
-            indegree[child]--;
-            if(indegree[child]==1){
-                q.push(child);
+        queue<int>q;
+        for(int i=0;i<n;i++)
+            if(degree[i]==1)q.push(i);
+            while(!q.empty()){
+                int size=q.size();
+                if(size==n)break;
+                while(size--){
+                    int curr=q.front();
+                    q.pop();
+                    n--;
+                    for(int next:adj[curr]){
+                        if(--degree[next]==1)q.push(next);
+                    }
+                }
             }
-        }
-    }
-}
-if(n==1) ans.push_back(0);
-return ans;
+            vector<int>result;
+            while(!q.empty()){
+                int curr=q.front();
+                q.pop();
+                result.push_back(curr);
+            }
+            return result;
+        
+        
     }
 };
